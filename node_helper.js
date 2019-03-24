314bd4ad36edbd8cf501fad2d159c00801ff071d
DESKTOP-PHVO08M\admin <divya1nigam@gmail.com>
2019-03-25 08:30:27 +1100

    fix issues

--- node_helper.js
+++ node_helper.js
@@ -1,59 +1,55 @@
-const request = require("request");
 const csv = require('csv-parser')
 const fs = require('fs')
 const loki = require('lokijs')
 const results = [];
 
 
-var db = new loki('stops.db', {
-    autoload: true,
-    autoloadCallback : databaseInitialize,
-    autosave: true, 
-    autosaveInterval: 4000
-});
+var db = new loki('stops.db');
 
-// implement the autoloadback referenced in loki constructor
-function databaseInitialize() {
-  var stops = db.getCollection("stops");
-  if (stops === null) {
+
+ 
     var stops = db.addCollection('stops');
-    fs.createReadStream('sydneytrains_GTFS_20190323140100//stop_times.txt')
+    fs.createReadStream('stop_times.txt')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
      stops.insert(results);
      runProgramLogic();
     });
-  }
-  else{
-    runProgramLogic();
-  }
+
 
   // kick off any program logic or start listening to external events
 
-}
+
 
 // example method with any bootstrap logic to run after database initialized
 function runProgramLogic() {
   var entryCount = db.getCollection("stops").count();
   var stops = db.getCollection("stops");
   console.log("number of entries in database : " + entryCount);
-  var stop1=stops.chain().find({'stop_id' : {'$contains':'2135'}}).branch();
-  var stop2=stops.chain().find({'stop_id' : {'$contains':'2121'}}).branch();
+  var stop1=stops.chain().find({'stop_id' : {'$contains':'2135'}}).where(function(obj){
+    return obj.stop_id.startsWith('2135');
+  });
+  var stop2=stops.chain().find({'stop_id' : {'$contains':'2121'}}).where(function(obj){
+    return obj.stop_id.startsWith('2121');
+  });
 
   var mapfun = function(left, right) {
     return {
       trip_id: left.trip_id,
-      stop1_arrival_time: left.arrival_time,
-      stop2_arrival_time: right.arrival_time,
-      stop1_sequence: left.stop_sequence,
-      stop2_sequence: right.stop_sequence,
+      stop2_arrival_time: left.arrival_time,
+      stop1_arrival_time: right.arrival_time,
+      stop2_sequence: left.stop_sequence,
+      stop1_sequence: right.stop_sequence,
     };
   };
-  //console.log(stop2);
+  //console.log(stop1.simplesort('trip_id').data());
+  //console.log(stop2.data());
 
-  var commonRoutes = stop1.eqJoin(stop2, "trip_id", "trip_id", mapfun).data();
-  var filteredRoutes = stop1.find({'$and':[{stop1_sequence: { $ne: undefined }},{stop1_sequence: {$ne:undefined}}] }).data();
+  var commonRoutes = stop2.eqJoin(stop1, "trip_id", "trip_id", mapfun);
+  var filteredRoutes = commonRoutes.where(function(obj){
+    return obj.stop1_sequence<obj.stop2_sequence;
+  }).data();
 
  console.log(filteredRoutes);
 }
